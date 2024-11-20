a3438b1c7188e9f780cb82178dff4664b9c601a5
YOUNGHO0 <31pqopq13@gmail.com>
2024-11-20 15:15:29 +0900

    Feat : 프로젝트 생성

---
+++ veda4backend/controllers/TestController.cc
@@ -0,0 +1,6 @@
+#include "TestController.h"
+
+void TestController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+}
