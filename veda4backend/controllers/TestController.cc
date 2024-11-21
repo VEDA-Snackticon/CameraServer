40c7acee45ca2d45a5f52a1443b0ae61375b0f02
YOUNGHO0 <31pqopq13@gmail.com>
2024-11-21 16:59:22 +0900

    Feat : controller 생성

--- veda4backend/controllers/TestController.cc
+++ veda4backend/controllers/TestController.cc
@@ -3,4 +3,9 @@
 void TestController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
 {
     // write your application logic here
+    auto resp=HttpResponse::newHttpResponse();
+    resp->setStatusCode(k200OK);
+    resp->setContentTypeCode(CT_TEXT_HTML);
+    resp->setBody("Your Page Contents");
+    callback(resp);
 }
--- veda4backend/controllers/TestController.h
+++ veda4backend/controllers/TestController.h
@@ -9,6 +9,7 @@ class TestController : public drogon::HttpSimpleController<TestController>
   public:
     void asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback) override;
     PATH_LIST_BEGIN
+    PATH_ADD("/helloWorld",Get);
     // list path definitions here;
     // PATH_ADD("/path", "filter1", "filter2", HttpMethod1, HttpMethod2...);
     PATH_LIST_END
