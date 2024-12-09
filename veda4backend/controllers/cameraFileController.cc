ea7820283c6510dc429cf5a9a20f833538a49380
YOUNGHO0 <31pqopq13@gmail.com>
2024-12-09 15:19:32 +0900

    Refactor: CameraFileController 리팩토링

--- veda4backend/controllers/cameraFileController.cc
+++ veda4backend/controllers/cameraFileController.cc
@@ -8,152 +8,188 @@
 #include <drogon/HttpRequest.h>  // HttpRequest 헤더 추가
 #include <drogon/HttpResponse.h> // HttpResponse 헤더 추가
 
+std::string cameraFileController::getTransactionId(SafeStringMap<std::string> pairs) {
+    if(pairs.find("transactionId") != pairs.end()){
+        return pairs["transactionId"];
+    }
+    return "";
+}
+
+std::string cameraFileController::getDescription(SafeStringMap<std::string> pairs) {
+    std::string description;
+    if(pairs.find("description") != pairs.end()) {
+        return pairs["description"];
+    }
+    return "";
+}
+
+bool cameraFileController::checkParameter(std::function<void(const HttpResponsePtr &)> &&callback, std::string transactionId, std::string description, const std::vector<HttpFile> files ) {
+    if (transactionId.empty()) {
+        auto response = HttpResponse::newHttpResponse();
+        response->setStatusCode(k400BadRequest);
+        response->setBody("transactionId is missing.");
+        callback(response);
+        return true;
+    }
+
+    if (description.empty()) {
+        auto response = HttpResponse::newHttpResponse();
+        response->setStatusCode(k400BadRequest);
+        response->setBody("Description is missing.");
+        callback(response);
+        return true;
+    }
+
+    if (files.empty()) {
+        auto response = HttpResponse::newHttpResponse();
+        response->setStatusCode(k400BadRequest);
+        response->setBody("At least one MP4 file must be uploaded.");
+        callback(response);
+        return true;
+    }
+    return false;
+}
+
+std::string cameraFileController::setUploadPath() {
+    std::string uploadPath = drogon::app().getCustomConfig()["upload_path"].asString();
+    if (uploadPath.empty()) {
+        uploadPath = "./uploads";  // 기본값 설정 (config에 없을 경우)
+    }
+    return uploadPath;
+}
+
+bool cameraFileController::checkSaveFiles(std::function<void(const HttpResponsePtr &)> &&callback, std::vector<std::string> savedFiles) {
+    // MP4 파일이 하나 이상 업로드 되었는지 확인
+    if (savedFiles.size() == 0) {
+        auto response = HttpResponse::newHttpResponse();
+        response->setStatusCode(k400BadRequest);
+        response->setBody("At least one MP4 file must be uploaded.");
+        callback(response);
+        return true;
+    }
+    return false;
+}
+
+bool cameraFileController::checkCameraEvent(std::function<void(const HttpResponsePtr &)> &&callback, std::vector<drogon_model::veda4::CameraEvent> camera_events, std::string transactionId) {
+    if (camera_events.empty() ) {
+        auto response = HttpResponse::newHttpResponse();
+        response->setStatusCode(k400BadRequest);
+        response->setBody("No cameraEvents found.");
+        callback(response);
+        return true;
+    }
+    return false;
+}
+
+void cameraFileController::logFile(const HttpFile &file, std::string fileExtension) {
+    // 파일 정보 출력 (디버그용)
+    LOG_INFO << "File received: " << file.getFileName();
+    LOG_INFO << "File size: " << file.fileLength();
+    LOG_INFO << "File extension: " << fileExtension;
+    LOG_INFO << "MD5: " << file.getMd5();
+}
+
+std::string cameraFileController::getFileExtension(std::string fileExtension) {
+    // 확장자에서 점(.)을 제거 (예: ".mp4" -> "mp4")
+    if (!fileExtension.empty() && fileExtension[0] == '.') {
+        fileExtension = fileExtension.substr(1);  // 첫 번째 점을 제거
+    }
+    return fileExtension;
+}
+
+bool cameraFileController::checkCamera(std::function<void(const HttpResponsePtr &)> &&callback, std::vector<drogon_model::veda4::Camera> foundCameras) {
+    if(foundCameras.empty()) {
+        auto response = HttpResponse::newHttpResponse();
+        response->setStatusCode(k400BadRequest);
+        response->setBody("cannot find event Camera.");
+        callback(response);
+        return true;
+    }
+    return false;
+}
+
+drogon_model::veda4::CameraFile cameraFileController::setCameraFile(std::string transactionId, std::string uploadPath, std::string savedFilename, int64_t foundCameraId) {
+    drogon_model::veda4::CameraFile cameraFile;
+    cameraFile.setCamId(foundCameraId);
+    cameraFile.setTransactionId(transactionId);
+    cameraFile.setPath(uploadPath);
+    cameraFile.setFileName(savedFilename);
+    return cameraFile;
+}
+
+void cameraFileController::sendSuccessResponse(std::function<void(const HttpResponsePtr &)> &&callback) {
+    // 성공 응답
+    auto response = HttpResponse::newHttpResponse();
+    response->setStatusCode(k200OK);
+    response->setBody("MP4 files uploaded successfully with UUID.");
+    callback(response);
+}
+
+std::string cameraFileController::saveFile(std::string transactionId, std::string uploadPath,const HttpFile &file) {
+    std::string fileName = file.getFileName();
+    std::string fileExtension = getFileExtension(std::filesystem::path(fileName).extension().string());
+    logFile(file, fileExtension);
+    std::string savePath = uploadPath + "/" + transactionId + "_" + file.getFileName();
+    std::string savedFilename = transactionId + "_" + file.getFileName();
+    // 파일 저장
+    file.saveAs(savePath);
+    LOG_INFO << "File saved to: " << savePath;
+    return fileName;
+}
+
+bool cameraFileController::checkTransactionEventReallyExist(std::function<void(const HttpResponsePtr &)> &&callback, std::string transactionId,std::shared_ptr<orm::Transaction> transaction) {
+    //실제 transcation이 있는 건지 확인한다.
+    orm::Mapper<drogon_model::veda4::CameraEvent> cameraEventMapper(transaction);
+    std::vector<drogon_model::veda4::CameraEvent> camera_events = cameraEventMapper.findBy(orm::Criteria(drogon_model::veda4::CameraEvent::Cols::_transaction_id,orm::CompareOperator::EQ, transactionId));
+    if (checkCameraEvent(std::move(callback), camera_events, transactionId)) return true;
+    return false;
+}
+
 void cameraFileController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
 {
-
     try {
         // 요청을 파싱
+        auto transaction = app().getDbClient()->newTransaction();
         MultiPartParser fileParser;
         fileParser.parse(req);
-        std::string transactionId;
-        std::string description;
         auto pairs = fileParser.getParameters();
-
-        if(pairs.find("transactionId") != pairs.end()) {
-            transactionId = pairs["transactionId"];
-        }
-
-        if(pairs.find("description") != pairs.end()) {
-            description = pairs["description"];
-        }
-
-        if (transactionId.empty()) {
-            auto response = HttpResponse::newHttpResponse();
-            response->setStatusCode(k400BadRequest);
-            response->setBody("transactionId is missing.");
-            callback(response);
-            return;
-        }
-
-        if (description.empty()) {
-            auto response = HttpResponse::newHttpResponse();
-            response->setStatusCode(k400BadRequest);
-            response->setBody("Description is missing.");
-            callback(response);
-            return;
-        }
-
-        // 업로드된 파일 확인
+        std::string transactionId = getTransactionId(pairs);
+        std::string description = getDescription(pairs);
         const auto& files = fileParser.getFiles();
-        if (files.empty()) {
-            auto response = HttpResponse::newHttpResponse();
-            response->setStatusCode(k400BadRequest);
-            response->setBody("At least one MP4 file must be uploaded.");
-            callback(response);
-            return;
-        }
+        if (checkParameter(std::move(callback), transactionId, description,files )) return;
 
         // config.yaml에서 upload_path 가져오기
-        std::string uploadPath = drogon::app().getCustomConfig()["upload_path"].asString();
-        if (uploadPath.empty()) {
-            uploadPath = "./uploads";  // 기본값 설정 (config에 없을 경우)
-        }
-
+        std::string uploadPath = setUploadPath();
         // 디렉토리 존재 여부 확인 및 생성
         if (!std::filesystem::exists(uploadPath)) {
             std::filesystem::create_directories(uploadPath);
         }
 
-        std::string savedFilename;
         // 업로드된 파일 처리
-        bool mp4Found = false;
+        std::vector<std::string> savedFiles;
         for (const auto& file : files) {
             // 파일 확장자 검사 (MP4만 허용)
-            std::string fileName = file.getFileName();
-            std::string fileExtension = std::filesystem::path(fileName).extension().string();  // 파일 확장자 추출
-
-            // 확장자에서 점(.)을 제거 (예: ".mp4" -> "mp4")
-            if (!fileExtension.empty() && fileExtension[0] == '.') {
-                fileExtension = fileExtension.substr(1);  // 첫 번째 점을 제거
-            }
-
-            if (fileExtension == "mp4") {
-                mp4Found = true;  // MP4 파일이 있음을 표시
-            } else {
-                // MP4 파일이 아니라면 무시
-                continue;
-            }
-
-            // 파일 정보 출력 (디버그용)
-            LOG_INFO << "File received: " << file.getFileName();
-            LOG_INFO << "File size: " << file.fileLength();
-            LOG_INFO << "File extension: " << fileExtension;
-            LOG_INFO << "MD5: " << file.getMd5();
-
-            // 파일 저장 경로 설정
-            std::string savePath = uploadPath + "/" + transactionId + "_" + file.getFileName();  // UUID를 파일 이름에 포함
-            savedFilename = transactionId + "_" + file.getFileName();
-            // 파일 저장
-            file.saveAs(savePath);
-            LOG_INFO << "File saved to: " << savePath;
+            std::string savedFilename = saveFile(transactionId, uploadPath, file);
+            savedFiles.push_back(savedFilename);
         }
+        if (checkSaveFiles(std::move(callback), savedFiles)) return;
+        if (checkTransactionEventReallyExist(std::move(callback), transactionId, transaction)) return;
 
-        // MP4 파일이 하나 이상 업로드 되었는지 확인
-        if (!mp4Found) {
-            auto response = HttpResponse::newHttpResponse();
-            response->setStatusCode(k400BadRequest);
-            response->setBody("At least one MP4 file must be uploaded.");
-            callback(response);
-            return;
-        }
-
-
-        auto client = app().getDbClient();
-        //실제 transcation이 있는 건지 확인한다.
-        orm::Mapper<drogon_model::veda4::CameraEvent> cameraEventMapper(client);
-
-        std::vector<drogon_model::veda4::CameraEvent> camera_events = cameraEventMapper.findBy(orm::Criteria(drogon_model::veda4::CameraEvent::Cols::_transaction_id,orm::CompareOperator::EQ, transactionId));
-
-        if (camera_events.empty()) {
-            auto response = HttpResponse::newHttpResponse();
-            response->setStatusCode(k400BadRequest);
-            response->setBody("No cameraEvents found.");
-            callback(response);
-            return;
-        }
         // transaction이 실제로 있다면 어떤 카메라가 보낸 요청인지 찾는다.
-        orm::Mapper<drogon_model::veda4::Camera> cameraMapper(client);
+        orm::Mapper<drogon_model::veda4::Camera> cameraMapper(transaction);
         std::vector<drogon_model::veda4::Camera> foundCameras = cameraMapper.findBy(orm::Criteria(drogon_model::veda4::Camera::Cols::_description,orm::CompareOperator::EQ,description));
-        if(foundCameras.empty()) {
-            auto response = HttpResponse::newHttpResponse();
-            response->setStatusCode(k400BadRequest);
-            response->setBody("cannot find event Camera.");
-            callback(response);
-            return;
-        }
-
+        if (checkCamera(std::move(callback), foundCameras)) return;
         // 찾아낸 카메라 번호를 가져온다.
         int64_t foundCameraId = foundCameras[0].getValueOfId();
 
+        for (std::string savedFilename : savedFiles) {
+            // 카메라 파일 이름과 함께 파일에 저장한다.
+            drogon_model::veda4::CameraFile cameraFile = setCameraFile(transactionId, uploadPath, savedFilename, foundCameraId);
+            orm::Mapper<drogon_model::veda4::CameraFile> cameraFileMapper(transaction);
+            cameraFileMapper.insert(cameraFile);
+        }
 
-        // 카메라 파일 이름과 함께 파일에 저장한다.
-
-        drogon_model::veda4::CameraFile cameraFile;
-
-        cameraFile.setCamId(foundCameraId);
-        cameraFile.setTransactionId(transactionId);
-        cameraFile.setPath(uploadPath);
-        cameraFile.setFileName(savedFilename);
-
-        orm::Mapper<drogon_model::veda4::CameraFile> cameraFileMApper(client);
-        cameraFileMApper.insert(cameraFile,[](const drogon_model::veda4::CameraFile& cameraFile){ std::cout <<"success" << std::endl;}, [](const orm::DrogonDbException){std::cout << "fail" << std::endl;});
-
-
-        // 성공 응답
-        auto response = HttpResponse::newHttpResponse();
-        response->setStatusCode(k200OK);
-        response->setBody("MP4 files uploaded successfully with UUID.");
-        callback(response);
+        sendSuccessResponse(std::move(callback));
     } catch (const std::exception& e) {
         // 에러 처리
         auto response = HttpResponse::newHttpResponse();
@@ -161,4 +197,5 @@ void cameraFileController::asyncHandleHttpRequest(const HttpRequestPtr& req, std
         response->setBody(std::string("Error: ") + e.what());
         callback(response);
     }
+
 }
--- veda4backend/controllers/cameraFileController.h
+++ veda4backend/controllers/cameraFileController.h
@@ -2,11 +2,46 @@
 
 #include <drogon/HttpSimpleController.h>
 
+#include "Camera.h"
+#include "CameraEvent.h"
+#include "CameraFile.h"
+
 using namespace drogon;
 
 class cameraFileController : public drogon::HttpSimpleController<cameraFileController>
 {
   public:
+    std::string getTransactionId(SafeStringMap<std::string> pairs);
+
+    std::string getDescription(SafeStringMap<std::string> pairs);
+
+    bool checkParameter(std::function<void(const HttpResponsePtr &)> &&callback, std::string transactionId,
+                        std::string description, std::vector<HttpFile> files);
+
+    std::string setUploadPath();
+
+    bool checkSaveFiles(std::function<void(const HttpResponsePtr &)> &&callback, std::vector<std::string> savedFiles);
+
+    bool checkCameraEvent(std::function<void(const HttpResponsePtr &)> &&callback,
+                          std::vector<drogon_model::veda4::CameraEvent> camera_events, std::string transactionId);
+
+    void logFile(const HttpFile &file, std::string fileExtension);
+
+    std::string getFileExtension(std::string fileExtension);
+
+    bool checkCamera(std::function<void(const HttpResponsePtr &)> &&callback,
+                     std::vector<drogon_model::veda4::Camera> foundCameras);
+
+    drogon_model::veda4::CameraFile setCameraFile(std::string transactionId, std::string uploadPath, std::string savedFilename,
+                                                  int64_t foundCameraId);
+
+    void sendSuccessResponse(std::function<void(const HttpResponsePtr &)> &&callback);
+
+    std::string saveFile(std::string transactionId, std::string uploadPath, const HttpFile &file);
+
+    bool checkTransactionEventReallyExist(std::function<void(const HttpResponsePtr &)> &&callback,
+                                          std::string transactionId, std::shared_ptr<orm::Transaction> transaction);
+
     void asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback) override;
     PATH_LIST_BEGIN
     // list path definitions here;
